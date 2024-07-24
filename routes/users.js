b7b7ae8538ccaa2149e4232c493aea56dd8e2ec4
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-07-24 11:48:05 +0500

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../models/Users');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 router.get('/signup', (req, res) => {
   res.render('pages/signup',{layout:false});
 });
